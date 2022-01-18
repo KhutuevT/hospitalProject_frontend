ce080e89e95a5951d10406515bdf107129bfd007
Tamerlan Khutuev <t.khutuev@gmail.com>
2022-01-18 14:15:10 +0300

    fix

--- hospital_project/src/components/VisitListComponent/VisitListComponent.js
+++ hospital_project/src/components/VisitListComponent/VisitListComponent.js
@@ -13,7 +13,7 @@ import "./VisitListComponent.scss";
 
 const VisitListComponent = ({ visits }) => {
   const arrVisits = Array.from(visits);
-  const tableTitle = ["Имя","Врач","Дата","Жалобы",]
+  const tableTitle = ["Имя", "Врач", "Дата", "Жалобы"];
   return (
     <div className="visit-list-component">
       <TableContainer className="visit-list" component={Paper}>
@@ -27,23 +27,27 @@ const VisitListComponent = ({ visits }) => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {arrVisits.map(({patient_name, doc_name, date, complaints}, index) => (
-              <TableRow
-                key={index}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell align="center">{patient_name}</TableCell>
-                <TableCell align="center">{doc_name}</TableCell>
-                <TableCell align="center">{moment(date).format("MM-DD-YYYY")}</TableCell>
-                <TableCell align="center">{complaints}</TableCell>
-                <TableCell align="center">
-                  <div className="button-div">
-                    <img className="delete-img" src="/images/Delete.svg" />
-                    <img className="edit-img" src="/images/Edit.svg" />
-                  </div>
-                </TableCell>
-              </TableRow>
-            ))}
+            {arrVisits.map(
+              ({ patient_name, doc_name, date, complaints }, index) => (
+                <TableRow
+                  key={index}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell align="center">{patient_name}</TableCell>
+                  <TableCell align="center">{doc_name}</TableCell>
+                  <TableCell align="center">
+                    {moment(date).format("MM-DD-YYYY")}
+                  </TableCell>
+                  <TableCell align="center">{complaints}</TableCell>
+                  <TableCell align="center">
+                    <div className="button-div">
+                      <img className="delete-img" src="/images/Delete.svg" />
+                      <img className="edit-img" src="/images/Edit.svg" />
+                    </div>
+                  </TableCell>
+                </TableRow>
+              )
+            )}
           </TableBody>
         </Table>
       </TableContainer>
